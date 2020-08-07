ce92498a41fdd8a65b0c14f31690ee14451c98f5
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2020-08-07 21:58:07 +0700

    show pixel RGB

--- Tool_Support/Show_Position_Mouse.py
+++ Tool_Support/Show_Position_Mouse.py
@@ -1,10 +1,14 @@
 import pyautogui
 import time
+import keyboard
 
 print("-------**********---------")
-print("Hiển thị tọa độ hiện tại của chuột.")
-input("[Enter]")
-while True:
+print("Hiển thị tọa độ hiện tại của chuột:\n")
+print("Point(x, y)(R, G, B)\n")
+input("Continue:[Enter] ; Exit:[Esc]")
+while keyboard.is_pressed('esc') == False:
     Mouse_XY = pyautogui.position()
-    print(Mouse_XY)
+    print(Mouse_XY,end='')
+    pixel_RGB = pyautogui.pixel(Mouse_XY[0],Mouse_XY[1])
+    print(pixel_RGB)
     time.sleep(0.1)
