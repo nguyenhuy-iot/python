2fa757d64a11cc04e0cca2a79b9a6274e822856c
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2020-08-08 18:36:38 +0700

    08/08/2020, 18:36:32

---
+++ Tool_Support/Auto_Command_Git_Bash.py
@@ -0,0 +1,37 @@
+import pyautogui
+import time
+import keyboard
+
+def wait_key():
+    delay=0.2
+    while keyboard.is_pressed('enter') == False: pass
+    time.sleep(delay)
+def send_command(cmd):
+    print(cmd)
+    
+print("-------**********---------")
+print("cập nhật thư mục lên git hub")
+print("Continue:[Enter]")
+wait_key()
+
+print("git add .")
+pyautogui.write("git add .")
+wait_key()
+
+current_time = time.strftime("\"%d/%m/%Y, %H:%M:%S\"")
+print("git commit -m ",current_time)
+pyautogui.write("git commit -m ")
+pyautogui.write(current_time)
+wait_key()
+
+print("git push origin master")
+pyautogui.write("git push origin master")
+wait_key()
+
+input("Exit")
+
+'''
+git add .
+git commit -m "command"
+git push origin master
+'''
